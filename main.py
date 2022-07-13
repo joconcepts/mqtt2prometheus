4ffe38fdf53af185f73b09ac69a3c763758a4d29
Jonas Keidel <jonas@jonas-keidel.de>
2022-07-13 21:35:25 +0200

    Add regex group matching for labels

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Client:
         self.uuid = str(uuid.uuid4())
 
     def connect(self, on_message):
-        c = mqtt.Client(client_id=self.uuid)
+        c = mqtt.Client(client_id=f"mqtt2prom-{self.uuid}")
         c.username_pw_set(self.user, self.password)
         c.on_connect = self.on_connect
         c.on_message = on_message
@@ -38,17 +38,45 @@ class Exporter:
             name = metric['name']
             description = metric['description']
             labels = metric['labels']
+            labels.sort()
             self.metrics[name] = {
                 'prom': Gauge(name, description, labels),
-                'topics': metric['topics']
+                'topics': metric['topics'],
+                'labels': labels,
+                'label_mapping': metric.get('label_mapping', {})
             }
 
     def mqtt_handler(self, client, userdata, msg):
         for key, data in self.metrics.items():
             for topic in data['topics']:
-                if msg.topic == topic['topic']:
-                    value = msg.payload.decode()
-                    data['prom'].labels(*(topic['labels'].values())).set(value)
+                if topic.get('topic_regex', False):
+                    topic_regex = re.compile(topic['topic'])
+                    matched = topic_regex.match(msg.topic)
+                    if matched:
+                        value = msg.payload.decode()
+                        labels = {}
+
+                        for match_key, match_value in matched.groupdict().items():
+                            if match_key.startswith('label__'):
+                                labels[match_key.split('__')[1]] = match_value
+
+                        if data['label_mapping']:
+                            for label, label_config in data['label_mapping'].items():
+                                regex_group = label_config['regex_group']
+                                label_matched = matched[f"label__{regex_group}"]
+                                mapping = label_config['values'].get(label_matched)
+                                if not mapping:
+                                    next
+                                labels[label] = mapping
+
+                        if all(l is not None for l in list(labels.values())):
+                            data['prom'].labels(*labels.values()).set(value)
+                            
+
+                else:
+                    if msg.topic == topic['topic']:
+                        value = msg.payload.decode()
+                        data['prom'].labels(*(topic['labels'].values())).set(value)
 
 if __name__ == "__main__":
     config_file = sys.argv[1]
